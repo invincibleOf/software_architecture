0c373c4c688e47bbcbdfee9774b57c6ebbe6a3cc
invincibleOf <carlos.zambrano_@hotmail.com>
2026-05-24 12:56:33 -0500

    Se cambia texto mostrado para mejor comprension

--- src/main/java/co/edu/upb/patrones/ejercicio2/PlataformaEscritorio.java
+++ src/main/java/co/edu/upb/patrones/ejercicio2/PlataformaEscritorio.java
@@ -3,6 +3,6 @@ package co.edu.upb.patrones.ejercicio2;
 public class PlataformaEscritorio implements Plataforma{
     @Override
     public void mostrar(String titulo, String mensaje) {
-        System.out.println(titulo + " => " + mensaje);
+        System.out.println("[ESCRITORIO] " + titulo + " :: " + mensaje);
     }
 }
--- src/main/java/co/edu/upb/patrones/ejercicio2/PlataformaWeb.java
+++ src/main/java/co/edu/upb/patrones/ejercicio2/PlataformaWeb.java
@@ -3,6 +3,6 @@ package co.edu.upb.patrones.ejercicio2;
 public class PlataformaWeb implements Plataforma{
     @Override
     public void mostrar(String titulo, String mensaje) {
-        System.out.println(titulo + " => " + mensaje);
+        System.out.println("[WEB] " + titulo + ": " + mensaje);
     }
 }
